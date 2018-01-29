27f2b8f895777fbd003f62d5fad786c3321cc12b
Cody Van De Mark <cavigm@rit.edu>
2018-01-29 17:43:20 -0500

    updated with comments

--- src/responses.js
+++ src/responses.js
@@ -1,17 +1,60 @@
 const fs = require('fs');  // pull in the file system module
 
+//Load our index fully into memory. 
+//THIS IS NOT ALWAYS THE BEST IDEA.
+//We are using this for simplicity. Ideally we won't have
+//synchronous operations or load entire files into memory.
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 
+//function to send response
 const respond = (request, response, content, type) => {
+  //set status code (200 success) and content type
   response.writeHead(200, { 'Content-Type': type });
+  //write the content string or buffer to response
   response.write(content);
+  //send the response to the client
   response.end();
 };
 
+//function for our /cats page
+//Takes request, response and an array of client requested mime types
+const getCats = (request, response, acceptedTypes) => {
+  //object to send
+  const cat = {
+    name: 'tibbs',
+    age: 4,
+  };
+
+  //if the client's most preferred type (first option listed)
+  //is xml, then respond xml instead
+  if (acceptedTypes[0] === 'text/xml') {
+	//create a valid XML string with name and age tags.
+    let responseXML = '<response>';
+    responseXML = `${responseXML} <name>${cat.name}</name>`;
+    responseXML = `${responseXML} <age>${cat.age}</age>`;
+    responseXML = `${responseXML} </response>`;    
+
+	//return response passing out string and content type
+    return respond(request, response, responseXML, 'text/xml');
+  }
+
+  //stringify the json object (so it doesn't use references/pointers/etc)
+  //but is instead a flat string object.
+  //Then write it to the response.
+  const catString = JSON.stringify(cat);
+  
+  //return response passing json and content type
+  return respond(request, response, catString, 'application/json');
+};
+
+//function to handle the index page
 const getIndex = (request, response) => {
   respond(request, response, index, 'text/html');
 };
 
+//exports to set functions to public.
+//In this syntax, you can do getCats:getCats, but if they
+//are the same name, you can short handle to just getCats,
 module.exports = {
   getCats,
   getIndex,
--- src/server.js
+++ src/server.js
@@ -1,17 +1,38 @@
-const http = require('http');
-const url = require('url');
-const responseHandler = require('./responses.js');
+const http = require('http'); // pull in the http server module
+const url = require('url'); // pull in the url module
+//pull in our response handler file
+const responseHandler = require('./responses.js'); 
 
+//set the port. process.env.PORT and NODE_PORT are for servers like heroku
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+//key:value object to look up URL routes to specific functions
 const urlStruct = {
-
+  '/': responseHandler.getIndex,
+  '/cats': responseHandler.getCats,
+  index: responseHandler.getIndex,
 };
 
+//handle HTTP requests. In node the HTTP server will automatically
+//send this function request and pre-filled response objects.
 const onRequest = (request, response) => {
+  //parse the url using the url module
+  //This will let us grab any section of the URL by name
+  const parsedUrl = url.parse(request.url);
 
+  //grab the 'accept' headers (comma delimited) and split them into an array
+  const acceptedTypes = request.headers.accept.split(',');
+
+  //check if the path name (the /name part of the url) matches 
+  //any in our url object. If so call that function. If not, default to index.
+  if (urlStruct[parsedUrl.pathname]) {
+    urlStruct[parsedUrl.pathname](request, response, acceptedTypes);
+  } else {
+    urlStruct.index(request, response, acceptedTypes);
+  }
 };
 
+//start HTTP server 
 http.createServer(onRequest).listen(port);
 
 console.log(`Listening on 127.0.0.1: ${port}`);
